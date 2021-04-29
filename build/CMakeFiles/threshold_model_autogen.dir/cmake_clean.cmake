4422abde5f2cfe678a148180cb53c7bcfaaf9813
jdalmeida <jfdalmeida@wpi.edu>
2021-04-28 20:12:01 -0400

    initial code

---
+++ build/CMakeFiles/threshold_model_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "threshold_model_autogen"
+  "CMakeFiles/threshold_model_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/threshold_model_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/threshold_model_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
