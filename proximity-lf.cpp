4be9aa9f5a8b63f7f37470effabc2f2c29bd75b2
Nicholas Pacheco <nepacheco@wpi.edu>
2021-05-03 13:09:29 -0400

    Trying to get cpp code to compile with our desired data outputs

--- proximity-lf.cpp
+++ proximity-lf.cpp
@@ -1,6 +1,121 @@
 #include "proximity-lf.h"
+#include "buzz/buzzvm.h"
 
 /****************************************/
 /****************************************/
 
 REGISTER_LOOP_FUNCTIONS(CProximityLF, "proximity-lf");
+
+
+/**
+ * Functor to get data from the robots
+ */
+struct GetRobotData : public CBuzzLoopFunctions::COperation {
+
+   /** Constructor */
+   GetRobotData() {}
+
+   /** The action happens here */
+   virtual void operator()(const std::string& str_robot_id,
+                           buzzvm_t t_vm) {
+      /* Get the current number of valid neighbors */
+      buzzobj_t tValidNeighbors = BuzzGet(t_vm, "validNeighbors");
+      /* Make sure it's the type we expect (an integer) */
+      if(!buzzobj_isint(tValidNeighbors)) {
+         LOGERR << str_robot_id << ": variable 'task' has wrong type " << buzztype_desc[tValidNeighbors->o.type] << std::endl;
+         return;
+      }
+      /* Get the value */
+      int nValidNeighbors = buzzobj_getint(tValidNeighbors);
+      /* Set the mapping between robot id and number of valid neighbors */
+      m_validNeighbors[t_vm->robot] = nValidNeighbors;
+
+      /* Get the current number of invliad neighbors */
+      buzzobj_t tInvalidNeighbors = BuzzGet(t_vm, "invalidNeighbors");
+      /* Make sure it's the type we expect (an integer) */
+      if(!buzzobj_isint(tInvalidNeighbors)) {
+         LOGERR << str_robot_id << ": variable 'task' has wrong type " << buzztype_desc[tInvalidNeighbors->o.type] << std::endl;
+         return;
+      }
+      /* Get the value */
+      int nInvalidNeighbors = buzzobj_getint(tInvalidNeighbors);
+      /* Set the mapping between robot id and number of valid neighbors */
+      m_invalidNeighbors[t_vm->robot] = nInvalidNeighbors;
+
+
+      /* Get if a robot is stopped (aka count > 0) */
+      buzzobj_t tCount = BuzzGet(t_vm, "count");
+      /* Make sure it's the type we expect (an integer) */
+      if(!buzzobj_isint(tCount)) {
+         LOGERR << str_robot_id << ": variable 'task' has wrong type " << buzztype_desc[tCount->o.type] << std::endl;
+         return;
+      }
+      /* Get the value */
+      int nCount = buzzobj_getint(tCount);
+      /* Set the mapping between robot id and number of valid neighbors */
+      m_stopped[t_vm->robot] = nCount;
+   }
+
+   /** Store the number of robots that have "index" of valid neighbors */
+   std::map<int,int> m_validNeighbors;
+   std::map<int,int> m_invalidNeighbors;
+   /* Whether robots are stopped */
+   std::map<int,int> m_stopped;
+};
+
+void CProximityLF::Init(TConfigurationNode& t_tree) {
+   /* Call parent Init() */
+   CBuzzLoopFunctions::Init(t_tree);
+   /* Parse XML tree */
+   GetNodeAttribute(t_tree, "outfile", m_strOutFile);
+
+    /* Open the output file */
+   m_cOutFile.open(m_strOutFile.c_str(),
+                   std::ofstream::out | std::ofstream::trunc);
+}
+
+void CProximityLF::Reset() {
+   /* Reset the output file */
+   m_cOutFile.open(m_strOutFile.c_str(),
+                   std::ofstream::out | std::ofstream::trunc);
+
+}
+
+void CProximityLF::Destroy() {
+   m_cOutFile.close();
+}
+
+/****************************************/
+/****************************************/
+
+void CProximityLF::PostStep() {
+   /* Get robot data */
+   GetRobotData cGetRobotData();
+   BuzzForeachVM(cGetRobotData);
+   
+   /* Flush data to the output file */
+    for(int i = 0; i < GetNumRobots(); ++i) {
+        m_cOutFile << GetSpace().GetSimulationClock() << "\t"
+                << i << "\t"
+                << cGetRobotData.m_invalidNeighbors[i] << "\t"
+                << cGetRobotData.m_validNeighbors[i] << "\t"
+                << cGetRobotData.m_stopped[i];
+        m_cOutFile << std::endl;
+    }
+
+}
+
+/****************************************/
+/****************************************/
+
+bool CProximityLF::IsExperimentFinished() {
+   /* Feel free to try out custom ending conditions */
+   return false;
+}
+
+/****************************************/
+/****************************************/
+
+int CProximityLF::GetNumRobots() const {
+   return m_mapBuzzVMs.size();
+}
--- proximity-lf.h
+++ proximity-lf.h
@@ -2,6 +2,7 @@
 #define PROXIMITY_H
 
 #include <buzz/argos/buzz_loop_functions.h>
+#include <argos3/core/utility/math/rng.h>
 
 class CProximityLF : public CBuzzLoopFunctions {
 
@@ -9,6 +10,55 @@ public:
 
    CProximityLF() {}
    ~CProximityLF() override {}
+
+   /**
+    * Executes user-defined initialization logic.
+    * @param t_tree The 'loop_functions' XML configuration tree.
+    */
+   virtual void Init(TConfigurationNode& t_tree);
+
+   /**
+    * Executes user-defined reset logic.
+    * This method should restore the state of the simulation at it was right
+    * after Init() was called.
+    * @see Init()
+    */
+   virtual void Reset();
+
+   /**
+    * Executes user-defined logic right after a control step is executed.
+    */
+   virtual void PostStep();
+   
+   /**
+    * Returns true if the experiment is finished, false otherwise.
+    *
+    * This method allows the user to specify experiment-specific ending
+    * conditions. If this function returns false and a time limit is set in the
+    * .argos file, the experiment will reach the time limit and end there. If no
+    * time limit was set, then this function is the only ending condition.
+    *
+    * @return true if the experiment is finished.
+    */
+   virtual bool IsExperimentFinished();
+
+   /**
+    * Executes user-defined destruction logic.
+    * This method should undo whatever is done in Init().
+    * @see Init()
+    */
+   virtual void Destroy();
+
+   private:
+
+      int GetNumRobots() const;
+
+   private: 
+      /** The output file name */
+      std::string m_strOutFile;
+
+      /** The output file stream */
+      std::ofstream m_cOutFile;
 
 };
 
